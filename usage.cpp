a98088951fb1fb39ba14d89c4c23b68c4089f0ca
DIDdevelopers <abhijeet@localhost.localdomain>
2014-07-05 02:34:48 +0530

    combining all

--- usage.cpp
+++ usage.cpp
@@ -1,33 +1,43 @@
 #include "menuWidget.h"
+#include "searchInput.h"
 #include <ncurses.h>
 #include <iostream>
 #include <string>
+#include "findex.h"
+#include <vector>
+#define SEARCH_HT 3
+
 #define DEBUG(x) printf("ERROR: %s \n", x);
 using namespace std;
 int main()
 {
-
-	cMenu *mainWidget = new cMenu;
 	initscr();
 	cbreak();
 	keypad(stdscr, 1);
 	noecho();
 	refresh();
-	string sLine0 = "Tourist";
-	mainWidget->addToMenu(sLine0);
-	string sLine1 = "Hello";
-	mainWidget->addToMenu(sLine1);
-	string sLine2 = "GoodNight";
-	mainWidget->addToMenu(sLine2);
-	string sLine3 = "Good Afternoon";
-	mainWidget->addToMenu(sLine3);
-	mainWidget->show();
-	int nIn = getch();
+
+	cSearchWidget *pSearch = new cSearchWidget(SEARCH_HT, COLS, 0, 0);
+	cMenu *pMenu = new cMenu(LINES - SEARCH_HT + 1, COLS, 0, SEARCH_HT);
+	pSearch->show();
+//	pMenu->show();
+	cFileIndexer *pIndexer = new cFileIndexer("/home/abhijeet/music");
+	pIndexer->createIndex();
+	vector<string> vResult = pIndexer->searchIndex("arth");
+	for(vector<string>::iterator iter = vResult.begin(); iter < vResult.end(); iter++)
+	{
+		pMenu->addToMenu( *iter );
+	}
+	pMenu->show();
+/*	
+	int nIn = wgetch(pSearch->getWindow());
 	while(nIn != 'q')
 	{
-		mainWidget->onKeyPressed(nIn);
-		nIn = getch();
+		pMenu->onKeyPressed(nIn);
+		
+		nIn = wgetch(pSearch->getWindow());
 	}
+*/
 	getch();
 	endwin();
 }
